612fec7cf858444f43f92e4335e9a7d75d0cc07a
RahulML2505GitHub <rahulml2505ytc@gmail.com>
2021-05-27 16:20:17 +0530

    Direct Commit

--- isseu-solver.py
+++ isseu-solver.py
@@ -2,5 +2,10 @@ import os
 
 class Library(object):
     
-    def __init__(self):
+    def __init__(self,name,register):
+        self.name = name
+        self.register = register
+        self.books = self.register.keys()
+
+    def funcname(self, parameter_list):
         pass
